5a7e27e4d10f4e18887c7098e315d49a9490afdb
harlanfadhilah69 <harlancoegg@gmail.com>
2025-11-28 18:15:09 +0700

    menambahkan fungsi validasiInput di EntryViewModel

--- app/src/main/java/com/example/room_database/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/room_database/viewmodel/EntryViewModel.kt
@@ -16,3 +16,10 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel()
         var uiStateSiswa by mutableStateOf(UIStateSiswa())
             private set
         /* Fungsi untuk memvalidasi input */
+        private fun  validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa
+        ): Boolean {
+            return with(uiState) {
+                nama.isNotBlank() && alamat.isNotBlank() && telepon.isNotBlank()
+            }
+        }
+
