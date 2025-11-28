81cf2ea3f614f0ed2f1bc0ecf95f8865db606d70
harlanfadhilah69 <harlancoegg@gmail.com>
2025-11-28 17:33:00 +0700

    Definisi interface DestinasiNavigasi

---
+++ app/src/main/java/com/example/room_database/view/route/DestinasiNavigasi.kt
@@ -0,0 +1,12 @@
+package com.example.room_database.view.route
+
+interface DestinasiNavigasi {
+    /**
+     *
+     */
+    val route: String
+    /**
+     * String resource id yang berisi judul yang akan ditampilkan di layar halaman
+     */
+    val titleRes: Int
+}
