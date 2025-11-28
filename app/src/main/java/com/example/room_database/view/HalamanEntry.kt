4464f6050c31109841c5dbca80832faf0f749342
harlanfadhilah69 <harlancoegg@gmail.com>
2025-11-28 20:18:13 +0700

    menambah komponen Composable FormInputSiswa

--- app/src/main/java/com/example/room_database/view/HalamanEntry.kt
+++ app/src/main/java/com/example/room_database/view/HalamanEntry.kt
@@ -96,3 +96,11 @@ fun EntrySiswaBody(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
+){}
