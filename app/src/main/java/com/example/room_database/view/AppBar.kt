2362118e73302147b63c629ed7ab93c14d7b700b
harlanfadhilah69 <harlancoegg@gmail.com>
2025-11-28 18:57:31 +0700

    menambah tombol navigasi kembali kondisional di TopAppBar

--- app/src/main/java/com/example/room_database/view/AppBar.kt
+++ app/src/main/java/com/example/room_database/view/AppBar.kt
@@ -1,12 +1,18 @@
 package com.example.room_database.view
 
 import android.icu.text.CaseMap
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.room_database.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,7 +27,14 @@ fun SiswaTopAppBar(
         modifier = modifier,
         scrollBehavior = scrollBehavior,
         navigationIcon = {
-
+            if (canNavigateBack) {
+                IconButton (onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
         }
     )
 }
