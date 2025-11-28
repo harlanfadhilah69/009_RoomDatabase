48a4861cc34f0365392750792e06c2f151b8c099
harlanfadhilah69 <harlancoegg@gmail.com>
2025-11-28 18:18:08 +0700

    menambah data class DetailSIswa

--- app/src/main/java/com/example/room_database/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/room_database/viewmodel/EntryViewModel.kt
@@ -40,3 +40,10 @@ data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false
 )
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telepon: String = "",
+)
+
