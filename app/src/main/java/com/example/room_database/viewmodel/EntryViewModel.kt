2bcf147c6394228829860dfe2980eafa62373c34
harlanfadhilah69 <harlancoegg@gmail.com>
2025-11-28 17:40:21 +0700

    menmabahkan mutable state UIStateSiswa ke EntryViewModel

--- app/src/main/java/com/example/room_database/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/room_database/viewmodel/EntryViewModel.kt
@@ -1,9 +1,19 @@
 package com.example.room_database.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.room_database.repositori.RepositoriSiswa
+import com.example.room_database.room.Siswa
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel()
-{
+    {
+        /**
+         * Berisi status siswa saat ini
+         */
+        var uiStateSiswa by mutableStateOf(UIStateSiswa))
+            private set
+        /* Fungsi untuk memvalidasi input */
 
-}
+    }
