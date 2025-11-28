73546ec313c53c6b3c26116e287d0011ec1c2116
harlanfadhilah69 <harlancoegg@gmail.com>
2025-11-28 15:56:41 +0700

    menmabah antarmuka ContainerApp untuk injeksi dependensi

---
+++ app/src/main/java/com/example/room_database/repositori/ContainerApp.kt
@@ -0,0 +1,10 @@
+package com.example.room_database.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.room_database.room.DatabaseSiswa
+
+interface ContainerApp{
+    val repositoriSiswa : RepositoriSiswa
+}
+
