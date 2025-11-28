33c428e3bb831991df1cd7582e023736e23e3198
harlanfadhilah69 <harlancoegg@gmail.com>
2025-11-28 20:01:49 +0700

    menambah komponen SiswaApp dan inisialisasi HostNavigasi

--- app/src/main/java/com/example/room_database/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/room_database/view/uicontroller/PetaNavigasi.kt
@@ -1,12 +1,23 @@
 package com.example.room_database.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import java.lang.reflect.Modifier
+import com.example.room_database.view.EntrySiswaScreen
+import com.example.room_database.view.HomeScreen
+import com.example.room_database.view.route.DestinasiEntry
+import com.example.room_database.view.route.DestinasiHome
 
 @Composable
-fun SiswaApp(navController: NavController= rememberNavController(),
+fun SiswaApp(navController: NavHostController = rememberNavController(),
              modifier: Modifier){
-    HostNavigasi(navController= navController)
+    HostNavigasi(navController = navController)
 }
+
