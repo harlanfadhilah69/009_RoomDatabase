3399555406acae3f89244fe6abf0974f0dc0dda0
harlanfadhilah69 <harlancoegg@gmail.com>
2025-11-28 20:04:26 +0700

    menyelesaikan HostNavigasi dengan rute Home dan Entry

--- app/src/main/java/com/example/room_database/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/room_database/view/uicontroller/PetaNavigasi.kt
@@ -27,5 +27,17 @@ fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ){
-
+    NavHost(navController = navController, startDestination = DestinasiHome.route
+        , modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry
+                    .route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
+        }
+    }
 }
