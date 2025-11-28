3e657f7d6783b75caa7ecf044ab436a40ef8754c
harlanfadhilah69 <harlancoegg@gmail.com>
2025-11-28 15:34:29 +0700

    menyelesaikan logika inisialisasi database Singleton

--- app/src/main/java/com/example/room_database/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/room_database/room/DatabaseSiswa.kt
@@ -1,7 +1,9 @@
 package com.example.room_database.room
 
+import android.content.Context
 import android.provider.CalendarContract
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
@@ -11,6 +13,15 @@ abstract class DatabaseSiswa: RoomDatabase(){
     companion object{
         @Volatile
         private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instance?: synchronized(lock = this){
+                Room.databaseBuilder(
+                    context, klass = DatabaseSiswa::class.java,
+                    name = "siswa_database")
+                    .build().also { Instance=it }
+            })
+        }
     }
 }
 data class DatabaseSiswa()
