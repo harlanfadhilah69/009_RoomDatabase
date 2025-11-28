07488b8ba803b709e500ee97d3e160b9990a2ada
harlanfadhilah69 <harlancoegg@gmail.com>
2025-11-28 17:35:45 +0700

    menmabahkan kelas EntryViewModel

---
+++ app/src/main/java/com/example/room_database/viewmodel/EntryViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.room_database.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.room_database.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel()
+{
+
+}
