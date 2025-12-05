f33654be4db9ccd33b690d40b940e4a49ea496be
harlanfadhilah69 <harlancoegg@gmail.com>
2025-12-05 19:46:58 +0700

    salah memasukkan code, memperbaiki RepositoriSiswa.kt

--- app/src/main/java/com/example/room_database/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/room_database/repositori/RepositoriSiswa.kt
@@ -1,84 +1,27 @@
 package com.example.room_database.repositori
 
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.room_database.room.Siswa
 import com.example.room_database.room.SiswaDao
-import com.example.room_database.view.DetailSiswaScreen
-import com.example.room_database.view.EditSiswaScreen
-import com.example.room_database.view.EntrySiswaScreen
-import com.example.room_database.view.HomeScreen
-import com.example.room_database.view.route.DestinasiDetailSiswa
-import com.example.room_database.view.route.DestinasiEditSiswa
-import com.example.room_database.view.route.DestinasiEntry
-import com.example.room_database.view.route.DestinasiHome
 import kotlinx.coroutines.flow.Flow
 
-@Composable
-fun SiswaApp(
-    navController: NavHostController = rememberNavController(),
-    modifier: Modifier = Modifier
-) {
-    HostNavigasi(navController = navController, modifier = modifier)
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+
+    suspend fun deleteSiswa(siswa: Siswa)
+
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun HostNavigasi(
-    navController: NavHostController,
-    modifier: Modifier = Modifier
-) {
-    NavHost(
-        navController = navController,
-        startDestination = DestinasiHome.route,
-        modifier = modifier
-    ) {
-        // Rute untuk HomeScreen
-        composable(DestinasiHome.route) {
-            HomeScreen(
-                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
-                navigateToItemUpdate = { id ->
-                    navController.navigate("${DestinasiDetailSiswa.route}/${id}")
-                }
-            )
-        }
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa)  =siswaDao.insert(siswa)
 
-        // Rute untuk EntrySiswaScreen
-        composable(DestinasiEntry.route) {
-            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
-        }
-
-        // Rute untuk DetailSiswaScreen (dengan argumen ID)
-        composable(
-            route = DestinasiDetailSiswa.routeWithArgs,
-            arguments = listOf(navArgument(name = DestinasiDetailSiswa.itemIdArg) {
-                type = NavType.IntType
-            })
-        ) {
-            DetailSiswaScreen(
-                navigateToEditItem = { navController.navigate("${DestinasiEditSiswa.route}/${it}") },
-                navigateBack = { navController.navigateUp() }
-            )
-        }
-
-        // Rute untuk EditSiswaScreen (dengan argumen ID)
-        composable(
-            route = DestinasiEditSiswa.routeWithArgs,
-            arguments = listOf(navArgument(name = DestinasiEditSiswa.itemIdArg) {
-                type = NavType.IntType
-            })
-        ) {
-            EditSiswaScreen(
-                navigateBack = { navController.popBackStack() },
-                onNavigateUp = { navController.navigateUp() }
-            )
-        }
-    }
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
