d33c05d1144cce435083cd4449415c7c088da10f
harlanfadhilah69 <harlancoegg@gmail.com>
2025-12-05 14:58:52 +0700

    mengimplementasi  operasi CRUD (R-U-D) Siswa dan menambah suspend fun updateSiswa

--- app/src/main/java/com/example/room_database/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/room_database/repositori/RepositoriSiswa.kt
@@ -8,9 +8,11 @@ interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
 
-    fun getSiswaStream(id: Int): Flow<Siswa>
+    fun getSiswaStream(id: Int): Flow<Siswa?>
 
     suspend fun deleteSiswa(siswa: Siswa)
+
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -18,4 +20,8 @@ class OfflineRepositoriSiswa(
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa)  =siswaDao.insert(siswa)
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
