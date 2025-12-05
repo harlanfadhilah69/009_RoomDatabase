5e741b6c915735e499fbc66e0214d2dfe9d11e2d
harlanfadhilah69 <harlancoegg@gmail.com>
2025-12-05 18:50:25 +0700

    mendefinisikan objek destinasi navigasi untuk Edit Siswa di file DestinasiEditSiswa.kt

--- app/src/main/java/com/example/room_database/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/room_database/view/route/DestinasiEditSiswa.kt
@@ -1,4 +1,9 @@
 package com.example.room_database.view.route
 
-object DestinasiEditSiswa {
+import com.example.room_database.R
+object DestinasiEditSiswa: DestinasiNavigasi{
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg"
 }
