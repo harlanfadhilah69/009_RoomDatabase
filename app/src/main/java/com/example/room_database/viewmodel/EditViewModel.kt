816a9d0ff5cc39dc3165e26d20179e436b38c457
harlanfadhilah69 <harlancoegg@gmail.com>
2025-12-05 18:58:23 +0700

    membuat Edit view model dan menginisialisasi mutable UI state

--- app/src/main/java/com/example/room_database/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/room_database/viewmodel/EditViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.room_database.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.room_database.repositori.AplikasiSiswa
+import com.example.room_database.repositori.RepositoriSiswa
 
-class EditViewModel(SavedStateHandle: SavedStateHandle, repositoriSiswa: AplikasiSiswa) {
+class EditViewModel(
+    SavedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+        var uiStateSiswa by mutableStateOf(UIStateSiswa())
+            private set
+
 }
