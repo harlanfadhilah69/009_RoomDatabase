250a53e2c3f631d638f676d3c9b585173f9a5f1f
harlanfadhilah69 <harlancoegg@gmail.com>
2025-12-05 15:13:03 +0700

    menambah konstanta TIMEOUT_MILLIS di companion object

--- app/src/main/java/com/example/room_database/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/room_database/viewmodel/DetailViewModel.kt
@@ -32,3 +32,8 @@ class DetailViewModel (
         repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
     }
 
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
