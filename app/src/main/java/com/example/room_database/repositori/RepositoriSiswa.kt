0c4038832de966401a4e6834997ba59e3d91abb0
harlanfadhilah69 <harlancoegg@gmail.com>
2025-12-05 19:42:41 +0700

    menambah composable root SiswaApp dan inisialisasi NavHostController

--- app/src/main/java/com/example/room_database/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/room_database/repositori/RepositoriSiswa.kt
@@ -1,27 +1,31 @@
 package com.example.room_database.repositori
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.room_database.room.Siswa
 import com.example.room_database.room.SiswaDao
+import com.example.room_database.view.DetailSiswaScreen
+import com.example.room_database.view.EditSiswaScreen
+import com.example.room_database.view.EntrySiswaScreen
+import com.example.room_database.view.HomeScreen
+import com.example.room_database.view.route.DestinasiDetailSiswa
+import com.example.room_database.view.route.DestinasiEditSiswa
+import com.example.room_database.view.route.DestinasiEntry
+import com.example.room_database.view.route.DestinasiHome
 import kotlinx.coroutines.flow.Flow
 
-interface RepositoriSiswa {
-    fun getAllSiswaStream(): Flow<List<Siswa>>
-    suspend fun insertSiswa(siswa: Siswa)
-
-    fun getSiswaStream(id: Int): Flow<Siswa?>
-
-    suspend fun deleteSiswa(siswa: Siswa)
-
-    suspend fun updateSiswa(siswa: Siswa)
+@Composable
+fun SiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    HostNavigasi(navController = navController, modifier = modifier)
 }
 
-class OfflineRepositoriSiswa(
-    private val siswaDao: SiswaDao
-): RepositoriSiswa {
-    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
-    override suspend fun insertSiswa(siswa: Siswa)  =siswaDao.insert(siswa)
-
-    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
-    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
-    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
-}
