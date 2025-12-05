6ca498711741e772554b47bb083a1b8c5e5bcdf9
harlanfadhilah69 <harlancoegg@gmail.com>
2025-12-05 19:02:49 +0700

    menambah logika init untuk ambil ID dan muat data Siswa yang akan diubah

--- app/src/main/java/com/example/room_database/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/room_database/viewmodel/EditViewModel.kt
@@ -5,14 +5,28 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.savedstate.savedState
 import com.example.room_database.repositori.AplikasiSiswa
 import com.example.room_database.repositori.RepositoriSiswa
+import com.example.room_database.view.route.DestinasiDetailSiswa
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class EditViewModel(
-    SavedStateHandle: SavedStateHandle,
+    savedStateHandle: SavedStateHandle,
     private val repositoriSiswa: RepositoriSiswa): ViewModel() {
 
         var uiStateSiswa by mutableStateOf(UIStateSiswa())
             private set
-
+private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoriSiswa.getSiswaStream(idSiswa)
+                .filterNotNull()
+                .first()
+                .toUIStateSiswa()
+        }
+    }
 }
